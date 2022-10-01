c07c82133fcff20842f323700f8a78e768e6adf2
Diego Gean da Fé <diegogeandafe@gmail.com>
2022-10-01 14:49:28 -0300

    Atualizado as classes SampaFoodAdminServerApplication e SampaFoodAdminServerApplicationTests

--- src/main/java/br/com/sampafood/admin/server/SampaFoodAdminServerApplication.java
+++ src/main/java/br/com/sampafood/admin/server/SampaFoodAdminServerApplication.java
@@ -1,4 +1,4 @@
-package br.com.algafood.admin.server.sampafoodadminserver;
+package br.com.sampafood.admin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/sampafood/admin/server/SampaFoodAdminServerApplicationTests.java
+++ src/test/java/br/com/sampafood/admin/server/SampaFoodAdminServerApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.algafood.admin.server.sampafoodadminserver;
+package br.com.sampafood.admin.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
